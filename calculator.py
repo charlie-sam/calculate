19833aba7812db0dc5ebf447e9fc6ecb4471c1d8
Charlie <murdaresambhaji4@gmail.com>
2023-09-15 19:46:21 +0530

    first try

---
+++ calculator.py
@@ -0,0 +1,13 @@
+first_value = int(input("enter first value: "))
+second_value = int(input("enter first second: "))
+
+print("which operation do you perform")
+print("+","-","*","/")
+operation = input(" ")
+def cal():
+    if operation == "+":
+       a = (first_value + second_value)
+    print(a)
+cal()
+
+    
