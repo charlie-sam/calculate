1157c22629d4bb3e0af00254a05102f1b3b0add1
Charlie <murdaresambhaji4@gmail.com>
2023-09-16 23:53:08 +0530

    complete calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,37 @@
-first_value = int(input("enter first value: "))
-second_value = int(input("enter first second: "))
+print('''
+a - add
+b - sub
+c - mul
+d - div
+''')
 
-print("which operation do you perform")
-print("+","-","*","/")
-operation = input(" ")
-def cal():
-    if operation == "+":
-       a = (first_value + second_value)
-    print(a)
-cal()
+def x():
+    if operation == 'a':
+         return "+"
+    elif operation == 'b':
+         return "-"
+    elif operation == "c":
+         return "*"
+    elif operation == "d":
+         return "/"
+while True:
+    again = (input("Do you want to perform operation(y/n): "))
+    if again == 'n':
+        break
 
-    
+    elif again == 'y':
+
+        operation = (input("which operation do you perform > "))
+
+        a = int(input("enter your first value > "))
+        b = int(input("enter your second value > "))
+            
+        if operation == 'a':
+            c = (a+b)
+        elif operation == 'b':
+            c = (a-b)
+        elif operation == 'c':
+            c = (a*b)
+        elif operation == 'd':
+            c = (a/b)     
+        print("your ans =:",a, x(), b,'=',c )
